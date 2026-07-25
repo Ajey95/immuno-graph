cc7ab255cc67b70abc9a953587d194ca35e7215a
vtg2208 <theegelavishnuvardhan22@gmail.com>
2026-07-26 02:23:06 +0530

    fix: configure local openbabel data path

--- scripts/science/obabel.cmd
+++ scripts/science/obabel.cmd
@@ -1,3 +1,5 @@
 @echo off
 setlocal
+set "BABEL_DATADIR=%~dp0..\..\.venv-science\Lib\site-packages\openbabel\bin\data"
+set "OPENBABEL_DATADIR=%BABEL_DATADIR%"
 "%~dp0..\..\.venv-science\Scripts\obabel.exe" %*
